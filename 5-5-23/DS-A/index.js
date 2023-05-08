73047ab601ce73424311ac2cb45c58210b33b20c
goosey-goose <g.ebenezer.miranda@gmail.com>
2023-05-07 20:47:33 -0400

    current state

--- 5-5-23/DS-A/index.js
+++ 5-5-23/DS-A/index.js
@@ -1,23 +1,56 @@
 //  STRUCTY, "GRAPHS: Connected Components Count"
 
 const connectedComponentsCount = (graph) => {
+
+
+  const traverseAndCountComponents = (stack, visited, count) => {
+    let countThis = false;
+
+    while (stack.length > 0) {
+        countThis = true;
+        const current = stack.pop();
+        if (visited.has(current)) continue;
+        visited.add(current);
+
+        for (let neighbor of graph[current]) {
+            stack.push(neighbor);
+        }
+
+        delete graph[`${current}`];
+
+        if (stack.length === 0 && Object.keys(graph).length > 0) {
+            Object.keys(graph)
+        }
+    }
+
+    if (countThis) ++count;
+
+    return count;
+  }
+
+  let count = 0;
+
   let nodes = Object.keys(graph).map((key) => parseInt(key));
 
   const stack = [ nodes[0] ];
 
   let visited = new Set();
 
-    while (stack.length > 0) {
-        const current = stack.pop();
-        if (visited.has(current)) continue;
-        console.log(current);
-        visited.add(current);
+  let result;
 
-        for (let neighbor of graph[current]) {
-            stack.push(neighbor);
-        }
-    }
+  if (Object.keys(graph).length > 0)
 };
+
+
+
+
+
+
+
+
+
+
+
 
 
 
