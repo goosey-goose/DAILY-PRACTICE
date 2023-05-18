c6356cff9667e7902cf6eece5317fd32cb47e054
Gustavo Miranda <g.ebenezer.miranda@gmail.com>
2023-05-18 01:47:03 +0000

    add ListNode definition and multiple created nodes

--- 5-17-23/mergeTwoLists.js
+++ 5-17-23/mergeTwoLists.js
@@ -1,3 +1,19 @@
+//  Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+} 
+
+const list1 = [1,2,4], list2 = [1,3,4];
+
+const four = new ListNode(4)
+const two = new ListNode(2, four)
+const one = new ListNode(1, two)
+
+const four_4 = new ListNode(4)
+const three_3 = new ListNode(3, four_4)
+const one_1 = new ListNode(1, three_3)
+
 var mergeTwoLists = function(list1, list2) {
     if (list1 === null && list2 !== null) return list2
     if (list2 === null && list1 !== null) return list1
