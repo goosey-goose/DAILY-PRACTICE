a48599a4e5d857771f283309a44d7586cc2820ef
Gustavo Miranda <g.ebenezer.miranda@gmail.com>
2023-05-18 02:24:23 +0000

    first semi working version

--- 5-17-23/mergeTwoLists.js
+++ 5-17-23/mergeTwoLists.js
@@ -29,19 +29,21 @@ var mergeTwoLists = function(list1, list2) {
             tail.next = list1
             tail = list1
             list1 = list1.next
+            console.log(tail.val)
         }
 
         if (list1.val > list2.val) {
             tail.next = list2
             tail = list2
             list2 = list2.next
+            console.log(tail.val)
         }
-
-        
-
-        list1 = list1.next
-        list2 = list2.next
     }
+
+    if (list1 === null) list2.next = list1
+    if (list2 === null) list1.next = list2
+
+    return dummy.next
 };
 
-mergeTwoLists(one, one_1);
+console.log(mergeTwoLists(one, one_1));
