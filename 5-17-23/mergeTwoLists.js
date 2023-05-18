9eb7ba61f66880549aa12af8d585a7d98c2d5957
Gustavo Miranda <g.ebenezer.miranda@gmail.com>
2023-05-18 02:36:35 +0000

    complete a working version

--- 5-17-23/mergeTwoLists.js
+++ 5-17-23/mergeTwoLists.js
@@ -1,18 +1,19 @@
 //  Definition for singly-linked list.
-function ListNode(val, next) {
+function ListNode(val, next, list) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
+    this.list = list
 } 
 
 const list1 = [1,2,4], list2 = [1,3,4];
 
-const four = new ListNode(4)
-const two = new ListNode(2, four)
-const one = new ListNode(1, two)
+const four = new ListNode(4, null, "List 1")
+const two = new ListNode(2, four, "List 1")
+const one = new ListNode(1, two, "List 1")
 
-const four_4 = new ListNode(4)
-const three_3 = new ListNode(3, four_4)
-const one_1 = new ListNode(1, three_3)
+const four_4 = new ListNode(4, null, "List 2")
+const three_3 = new ListNode(3, four_4, "List 2")
+const one_1 = new ListNode(1, three_3, "List 2")
 
 var mergeTwoLists = function(list1, list2) {
     if (list1 === null && list2 !== null) return list2
@@ -29,14 +30,12 @@ var mergeTwoLists = function(list1, list2) {
             tail.next = list1
             tail = list1
             list1 = list1.next
-            console.log(tail.val)
-        }
-
-        if (list1.val > list2.val) {
+            // console.log(tail.val, " ", tail.list)
+        } else if (list1.val > list2.val) {
             tail.next = list2
             tail = list2
             list2 = list2.next
-            console.log(tail.val)
+            // console.log(tail.val, " ", tail.list)
         }
     }
 
