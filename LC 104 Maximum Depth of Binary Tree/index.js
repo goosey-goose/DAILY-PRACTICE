f5522016a6374bdaa4e86371e06c22f588f23cd4
goosey-goose <g.ebenezer.miranda@gmail.com>
2023-05-24 15:51:26 -0400

    first draft of full code for 104

--- LC 104 Maximum Depth of Binary Tree/index.js
+++ LC 104 Maximum Depth of Binary Tree/index.js
@@ -1,12 +1,12 @@
-var maxDepth = function(root, depth = 1, max = 0) {
-    if (root === null) return 0
+var maxDepth = function(root, depth = 1, max = [0]) {
+    if (root === null) return max[0]
 
-    if (depth > max) {
-        max = depth
+    if (depth > max[0]) {
+        max[0] = depth
     }
 
     const left = maxDepth(root.left, depth + 1, max)
     const right = maxDepth(root.right, depth + 1, max)
 
-    return max
+    return max[0]
 }
